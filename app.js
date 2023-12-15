3fb1ccf0b2666d7efa813f8a8915a8e162a57301
rash010 <rashmi@sensarobotics.com>
2023-12-15 12:49:02 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,25 @@
+const heading = React.createElement(
+    "h1",
+    {
+        id:"heading"
+    }, 
+"hello from react")
+const parent = React.createElement(
+    "div",
+    {
+        id:"parent"
+    },
+    React.createElement(
+        "div",
+        {
+            id:"child"
+        },
+        [React.createElement("h1",{}, "hellooo h1 I am nested elements"),
+        React.createElement("h2",{}, "hellooo h2 I am nested elements")]
+    )
+    )
+
+    //jsx
+console.log(parent); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
